5c8e285da9a58c8a3b26c6c716b19b38821be5b3
Lupoldo <alexhprogram@gmail.com>
2025-10-27 20:05:44 +0100

    Init CenaFilosofos v1.0

--- src/Filosofo.java
+++ src/Filosofo.java
@@ -2,17 +2,69 @@ public class Filosofo implements Runnable{
     //Atributos
     private int comensal;
     private Mesa mesa;
+    private Thread hilo;
 
     //Constructor
     public Filosofo(int comensal, Mesa mesa){
         this.comensal=comensal;
         this.mesa=mesa;
+        hilo = new Thread(this,"Filosofo"+comensal);
     }
 
     //Metodos
-    public void pensar(){}
-    public void comer(){}
+    public void pensar(){
+        System.out.println("El filosofo "+getComensal()+" está pensando...");
+        try {
+            hilo.sleep(random(1,5000));
+        } catch (InterruptedException e) {}
+    }
+
+    public void comer(){
+        System.out.println("El filosofo "+getComensal()+" está comiendo...");
+        try {
+            hilo.sleep(random(1,2000));
+        } catch (InterruptedException e) {}
+    }
+
+    public int getComensal(){
+        return comensal;
+    }
+
+    public Thread getHilo(){
+        return hilo;
+    }
+
+    private int random(int limitMin, int limitMax){
+        return ((int) (Math.random() * (limitMax-limitMin+1)) + limitMin);
+    }
 
     @Override
-    public void run(){}
+    public void run(){
+        while(true){
+          switch (String.valueOf(random(0,1))){
+              case "0"->{ //comer (si puede sino piensa hasta comer)
+                  boolean salida = true;
+                  while(salida){
+                      if(mesa.cantidadTicket() > 0){
+                          if(mesa.tenederoIzquierdo(comensal)==0 && mesa.tenedorDerecha(comensal)==0) {
+                              mesa.cogerTenedor(comensal);
+                              comer();
+                              mesa.dejarTenedores(comensal);
+                              System.out.println("Suelta los tenedores filosofo"+getComensal());
+                              salida = false;
+                          }
+                      } else {
+                          pensar();
+                      }
+                  }
+              }
+              case "1"->{ //pensar
+                  pensar();
+              }
+              default ->{ //error fatal
+                  System.out.println("Se le ha parado el corazon al Filosofo");
+              }
+          }
+        }
+    }
 }
--- src/Mesa.java
+++ src/Mesa.java
@@ -1,22 +1,70 @@
+import java.util.concurrent.Semaphore;
+
 public class Mesa {
     //Atributos
+    private static final int filosofos = 5;
     private boolean[] tenedores;
+    // 0 1 2 3 4 5 filosofos
+    //0 1 2 3 4 5 0 tenedores
+    private Filosofo[] list_filosofos = new Filosofo[filosofos];
+    private Semaphore semaforo = new Semaphore((int) filosofos/2,true);
 
     //Constructor
-    public Mesa(){}
+    public Mesa(){
+        tenedores= new boolean[filosofos];
+    }
 
     //Metodos
-    public void cogerTenedor(int n){}
-    public int tenedorDerecha(int n){
-        return 0;
+    public void cogerTenedor(int comensal){
+        try {
+            semaforo.acquire();
+            tenedores[comensal]=true;
+            if(comensal+1==filosofos){
+                tenedores[0]=true;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public int tenederoIzquierdo(int n){
-        return 0;
+    public int tenedorDerecha(int comensal){
+        int tenedor=1;
+        if(comensal+1==filosofos){
+            comensal=0;
+        }
+        if(!tenedores[comensal+1])
+            tenedor=0;
+        return tenedor;
     }
-    public void dejarTenedores(int n){}
+    public int tenederoIzquierdo(int comensal){
+        int tenedor=1;
+        if(!tenedores[comensal])
+            tenedor=0;
+        return tenedor;
+    }
+    public void dejarTenedores(int comensal){
+        tenedores[comensal]=false;
+        if(comensal+1==filosofos){
+            tenedores[0]=false;
+        }
+        semaforo.release();
+    }
+
+    //Metodos relacionados con el Semaforo
+    public int cantidadTicket(){
+        return semaforo.availablePermits();
+    }
 
     //Lanzador
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+        Mesa mesa = new Mesa();
+        mesa.comienzo(mesa,filosofos);
+    }
 
+    public void comienzo(Mesa mesa, int filosofos){
+        for(int i =0; i<=(filosofos-1); i++){
+            Filosofo filosofo= new Filosofo(i,mesa);
+            list_filosofos[i] = filosofo;
+            filosofo.getHilo().start();
+        }
     }
 }
